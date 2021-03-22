cc6a026e33e221fac9fbc95e318a23009560e2a0
Earl Landicho <earldaniellandicho@gmail.com>
2021-03-22 13:32:56 -0500

    Adding more stuff

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,23 +7,22 @@ namespace Player
 		[SerializeField] private float movementSpeed;
 		[SerializeField] private float jumpForce;
 
-		private Rigidbody2D playerRigidBody;
-
-		private void Awake()
-		{
-			playerRigidBody = GetComponent<Rigidbody2D>();
-		}
+		private Rigidbody2D playerRigidBody => GetComponent<Rigidbody2D>();
 
 		private void Update()
 		{
 			MoveCheck();
+			JumpCheck();
+		}
 
+		private void JumpCheck()
+		{
 			if (Input.GetButtonDown("Jump"))
 			{
 				playerRigidBody.AddForce(new Vector2(0, jumpForce));
 			}
 		}
-		
+
 		private void MoveCheck()
 		{
 			playerRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed,
